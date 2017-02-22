c62c1e815fb39107e3d932893d0254bb0191fb68
Andy Gauge <andygauge@gmail.com>
2017-02-22 13:29:05 -0800

    Gateway guessor deduces your network and broadcast addresses, and attempts to guess your gateway.

---
+++ src/ip/mod.rs
@@ -0,0 +1,52 @@
+pub fn is_valid(address: &str) -> bool {
+    let octets = address.split(".");
+    let mut octet_count: u8 = 0;
+    for byte in octets {
+        match byte.to_string().parse::<u8>() {
+        Err(e) => return false,
+        Ok(n) => octet_count += 1,
+        }
+        if octet_count == 4 {
+            return true
+        }
+    }
+    return false
+}
+
+pub fn make_mask_from_cidr(cidr: u8) -> u32 {
+    let mut mask: u32 = 0;
+    for x in 0..cidr {
+        mask += 2u32.pow(31-x as u32);
+    }
+    return mask
+}
+
+pub fn make_mask_from_string(address: &str) -> u32 {
+    let octets = address.split(".");
+    let mut octet_count: u8 = 3;
+    let mut mask: u32 = 0;
+    for byte in octets {
+        let ibyte = byte.to_string().parse::<u8>().unwrap();
+        mask += 256u32.pow(octet_count as u32) * ibyte as u32;
+        if octet_count > 0 {
+            octet_count -= 1;
+        }
+    }
+    return mask
+}
+
+pub fn make_ip_from_mask(mask: u32) -> String {
+    let oct1 = ((mask & 0xFF000000) / 0xFFFFFF) as u8;
+    let oct2 = ((mask & 0xFF0000) / 0xFFFF) as u8;
+    let oct3 = ((mask & 0xFF00) /0xFF) as u8;
+    let oct4 = (mask & 0xFF) as u8;
+    format!("{}.{}.{}.{}", oct1, oct2, oct3, oct4)
+}
+pub fn guess_gateway(host: &str, subnet_mask: u32) {
+    let hostu32 = make_mask_from_string(host);
+    let network = hostu32 & subnet_mask;
+    let broadcast = hostu32 | !subnet_mask;
+    println!("The network address for this host is: {}", make_ip_from_mask(network));
+    println!("The broadcast address for this host is: {}", make_ip_from_mask(broadcast));
+    println!("I'm guessing the gateway is {} or {}", make_ip_from_mask(network+1), make_ip_from_mask(broadcast-1));
+}
---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use std::env;
+extern crate gateway;
+
+fn main() {
+
+    let args: Vec<String> = env::args().collect();
+    match args.len() {
+            2 => {
+                    let address_parts: Vec<&str> = args[1].split('/').collect();
+                    if address_parts.len() == 2 {
+                        if (gateway::ip::is_valid(address_parts[0]) && address_parts[1].to_string().parse::<u8>().unwrap() < 33) {
+                                gateway::ip::guess_gateway(address_parts[0], gateway::ip::make_mask_from_cidr(address_parts[1].to_string().parse::<u8>().unwrap()))
+                        } else {
+                            println!("you suck");
+                        }
+
+
+                    } else {
+                        panic!("you suck");
+                    }
+            },
+            3 => {
+                if gateway::ip::is_valid(&args[1]) {
+                    gateway::ip::guess_gateway(&args[1], gateway::ip::make_mask_from_string(&args[2]));
+                } else {
+                    println!("Host address should look like this: 192.168.0.1")
+                }
+            },
+            _ => {
+println!("Gateway Guesser
+Returns expected Gateway address for a given IP and subnet
+Usage:
+gateway IP/mask CIDR notation or
+gateway IP mask
+Examples:
+gateway 192.168.0.2/24
+gateway 192.168.0.2 255.255.255.0")
+            }
+    }
+}
